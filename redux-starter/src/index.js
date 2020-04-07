bb27ce6d1f17aaffe70b55f880a6db15fbf748d2
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-07 15:46:20 +0300

    3-Creating Middleware

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -16,15 +16,15 @@ store.subscribe(() => {
 });
 
 store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugResolved({ id: 1 }));
 
-store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
 
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(userAdded({ name: "User 1" }));
+// store.dispatch(userAdded({ name: "User 2" }));
 
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
 console.log(store.getState());
 
