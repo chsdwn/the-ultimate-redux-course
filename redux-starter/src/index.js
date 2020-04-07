37264834753d335d7207a66f81ff223881a4f2b0
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-07 15:56:24 +0300

    5-Dispatching Functions

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,35 +1,7 @@
 import configureStore from "./store/configureStore";
-import {
-  bugAdded,
-  bugAssignedToUser,
-  bugResolved,
-  getBugsByUser,
-  getUnresolvedBugs,
-} from "./store/bugs";
-import { projectAdded } from "./store/projects";
-import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("store changed");
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
 });
-
-store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// store.dispatch(userAdded({ name: "User 1" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-console.log(store.getState());
-
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-console.log(unresolvedBugs);
-
-const bugsByUser = getBugsByUser(1)(store.getState());
-console.log(bugsByUser);
