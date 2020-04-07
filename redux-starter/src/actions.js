9ac14910d76bb7958d75b7f725fd0950d6514083
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-07 12:56:56 +0300

    13-Solution

--- redux-starter/src/actions.js
+++ redux-starter/src/actions.js
@@ -9,10 +9,3 @@ export const bugResolved = (id) => ({
   type: actions.BUG_RESOLVED,
   payload: { id },
 });
-
-// export function bugAdded(description) {
-//   return {
-//     type: actions.BUG_ADDED,
-//     payload: { description },
-//   };
-// }
--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -14,13 +14,19 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
+
     case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
+
     case actions.BUG_RESOLVED:
-      const updatedState = [...state];
-      const bug = updatedState.find((b) => b.id === action.payload.id);
-      bug.resolved = true;
-      return [...updatedState];
+      // const updatedState = [...state];
+      // const bug = updatedState.find((b) => b.id === action.payload.id);
+      // bug.resolved = true;
+      // return [...updatedState];
+      return state.map((b) =>
+        b.id !== action.payload.id ? b : { ...b, resolved: true }
+      );
+
     default:
       return state;
   }
