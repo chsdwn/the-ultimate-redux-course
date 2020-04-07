939f6aafe7ef878611cbdff091f61ecd0ac3cf09
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-07 13:03:45 +0300

    3-Private Properties

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,2 @@
-import store from "./store";
-import { bugAdded, bugResolved } from "./actions";
-
-const dispatch = store.dispatch;
-
-dispatch(bugAdded("Bug 1"));
-console.log(store.getState());
-
-dispatch(bugAdded("Bug 2"));
-dispatch(bugResolved(1));
-console.log(store.getState());
+import store from "./customStore";
+console.log(store);
