cdaf0c15efd2bc69bc291394dd8bb6ba63ce4604
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-08 11:36:48 +0300

    4-API Middleware

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
+import api from "./middleware/api";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger("console"), toast],
+    middleware: [...getDefaultMiddleware(), api, logger("console"), toast],
   });
 }
