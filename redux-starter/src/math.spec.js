90c1660f6a0a67b099809dcae9aa2813a447c735
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-08 13:08:07 +0300

    4-Your First Unit Test

--- redux-starter/src/math.spec.js
+++ redux-starter/src/math.spec.js
@@ -1,1 +1,14 @@
-it("first test", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("isEven should return true if given an even number", () => {
+    // Function under test (SUT)
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it("isEven should return false if given an odd number", () => {
+    const result = isEven(3);
+    expect(result).toEqual(false);
+  });
+});
